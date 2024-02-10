92a591424206176dfbeb6758baf2fb2812086409
Fulminazzo <fulminazzo21@gmail.com>
2024-02-10 23:02:57 +0100

    Added various constructors

--- src/main/java/it/fulminazzo/fulmicollection/objects/Refl.java
+++ src/main/java/it/fulminazzo/fulmicollection/objects/Refl.java
@@ -1,11 +1,14 @@
 package it.fulminazzo.fulmicollection.objects;
 
 
+import it.fulminazzo.fulmicollection.utils.ExceptionUtils;
+import it.fulminazzo.fulmicollection.utils.ReflectionUtils;
 import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.sql.Ref;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Objects;
 
 /**
@@ -17,6 +20,28 @@ import java.util.Objects;
 @Getter
 public class Refl<T> {
     private final T object;
+
+    public Refl(final @NotNull String className, Object @Nullable ... parameters) {
+        this(className, ReflectionUtils.objectsToClasses(parameters), parameters);
+    }
+
+    public Refl(final @NotNull String className, final Class<?> @Nullable [] parameterTypes, Object @Nullable ... parameters) {
+        this(ReflectionUtils.getClass(className), parameterTypes, parameters);
+    }
+
+    public Refl(final @NotNull Class<T> clazz, Object @Nullable ... parameters) {
+        this(clazz, ReflectionUtils.objectsToClasses(parameters), parameters);
+    }
+
+    public Refl(final @NotNull Class<T> clazz, final Class<?> @Nullable [] parameterTypes, Object @Nullable ... parameters) {
+        try {
+            Constructor<T> constructor = ReflectionUtils.getConstructor(clazz, parameterTypes);
+            this.object = constructor.newInstance(parameters);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            ExceptionUtils.throwException(e);
+            throw new IllegalStateException("Unreachable code");
+        }
+    }
 
     public Refl(final @Nullable T object) {
         this.object = object;
--- src/test/java/it/fulminazzo/fulmicollection/objects/ReflTest.java
+++ src/test/java/it/fulminazzo/fulmicollection/objects/ReflTest.java
@@ -1,6 +1,11 @@
 package it.fulminazzo.fulmicollection.objects;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.function.Supplier;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
@@ -22,4 +27,23 @@ class ReflTest extends AbstractReflTest {
         assertEquals(this.testClass.toString(), this.refl.toString());
     }
 
+    @DisplayName("Test constructors")
+    static class ConstructorsTest extends AbstractReflTest {
+
+        private static Object[] getConstructors() {
+            return new Object[]{
+                    (Supplier<Refl<TestClass>>) () -> new Refl<>(TestClass.class.getCanonicalName(), "James"),
+                    (Supplier<Refl<TestClass>>) () -> new Refl<>(TestClass.class.getCanonicalName(), "James".getClass()),
+                    (Supplier<Refl<TestClass>>) () -> new Refl<>(TestClass.class, "James"),
+                    (Supplier<Refl<TestClass>>) () -> new Refl<>(TestClass.class, "James".getClass()),
+            };
+        }
+
+        @ParameterizedTest
+        @MethodSource("getConstructors")
+        void test(Supplier<Refl<TestClass>> supplier) {
+            assertEquals(this.refl, supplier.get());
+            assertEquals(this.refl.getObject(), supplier.get().getObject());
+        }
+    }
 }
