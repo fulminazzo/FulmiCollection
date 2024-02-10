dfed68da53383504e587d9fa51205d3925ea815c
Fulminazzo <fulminazzo21@gmail.com>
2024-02-10 21:17:13 +0100

    Added JavaDoc and inferred nullity

--- src/main/java/it/fulminazzo/fulmicollection/objects/ReflObject.java
+++ src/main/java/it/fulminazzo/fulmicollection/objects/ReflObject.java
@@ -26,155 +26,188 @@ public class ReflObject<T> {
         this(objectClass, ReflectionUtils.objectsToClasses(params), params);
     }
 
-    public ReflObject(Class<T> objectClass, Class<?>[] paramTypes, Object... params) {
-        T object = null;
-        try {
-            Constructor<T> constructor = ReflectionUtils.getConstructor(objectClass, paramTypes);
-            if (constructor == null)
-                throw new NoSuchMethodException(String.format("Constructor not found %s(%s)", objectClass.getName(),
-                        ReflectionUtils.classesToString(paramTypes)));
-            constructor.setAccessible(true);
-            object = constructor.newInstance(params);
-        } catch (NullPointerException | NoSuchMethodException | InstantiationException | IllegalAccessException |
-                 InvocationTargetException e) {
-            ExceptionUtils.throwException(e);
-        }
-        this.objectClass = objectClass;
-        this.object = object;
-    }
-
-    public ReflObject(String classPath, boolean initiate) {
-        Class<T> objectClass = null;
-        T object = null;
-        try {
-            objectClass = ReflectionUtils.getClass(classPath);
-            if (initiate) {
-                Constructor<T> constructor = ReflectionUtils.getConstructor(objectClass);
-                if (constructor == null)
-                    throw new NoSuchMethodException(String.format("Constructor not found %s()", objectClass.getName()));
-                constructor.setAccessible(true);
-                object = constructor.newInstance();
-            }
-        } catch (NullPointerException | NoSuchMethodException | InstantiationException | IllegalAccessException |
-                 InvocationTargetException e) {
-            ExceptionUtils.throwException(e);
-        }
-        this.objectClass = objectClass;
-        this.object = object;
-    }
-
-    public ReflObject(T object, Class<T> objectClass) {
-        if (objectClass == null) throw new IllegalArgumentException("Object class cannot be null");
-        this.objectClass = objectClass;
-        this.object = object;
-    }
-
-    public ReflObject(T object) {
-        this(object, (Class<T>) object.getClass());
-    }
-
-    public Object[] getArray(ReflObject<?>... contents) {
-        Object[] objects = Arrays.stream(contents)
-                .map(o -> o.getObject() == null ? o.getObjectClass() : o.getObject())
-                .toArray();
-        return getArray(objects);
-    }
-
-    public Object[] getArray(Object... contents) {
-        if (objectClass == null) return null;
-        Object[] array = getArray(contents.length);
-        for (int i = 0; i < contents.length; i++) Array.set(array, i, contents[i]);
-        return array;
-    }
-
-    public Object[] getArray(int size) {
-        return (Object[]) Array.newInstance(objectClass, size);
-    }
-
-    public Field getField(String name) {
-        if (objectClass == null) return null;
-        try {
-            Field field = ReflectionUtils.getField(objectClass, name);
-            if (field == null) throw new NoSuchFieldException(String.format("Field %s not found in class %s", name, objectClass));
-            return field;
-        } catch (NoSuchFieldException e) {
-            ExceptionUtils.throwException(e);
-            return null;
-        }
-    }
-
-    public Field getFieldNameless(Class<?> type) {
-        if (objectClass == null) return null;
-        try {
-            Field field = ReflectionUtils.getField(objectClass, type);
-            if (field == null) throw new NoSuchFieldException(String.format("Field of type %s not found in class %s", type, objectClass));
-            return field;
-        } catch (NoSuchFieldException e) {
-            ExceptionUtils.throwException(e);
-            return null;
-        }
-    }
-
-    public Field getFieldNameless(String typeName) {
-        if (objectClass == null) return null;
-        try {
-            Field field = ReflectionUtils.getFieldNameless(objectClass, typeName);
-            if (field == null) throw new NoSuchFieldException(String.format("Field of type %s not found in class %s", typeName, objectClass));
-            return field;
-        } catch (NoSuchFieldException e) {
-            ExceptionUtils.throwException(e);
-            return null;
-        }
-    }
-
-    public <O> void setField(String name, O object) {
-        if (this.object == null) return;
+    /**
+     * Sets field.
+     *
+     * @param <O>    the type parameter
+     * @param name   the name
+     * @param object the object
+     * @return this refl object
+     */
+    public <O> @NotNull ReflObject<T> setField(@NotNull String name, @Nullable O object) {
+        if (this.object == null) return this;
         Field field = getField(name);
-        if (field == null) return;
         try {
             field.setAccessible(true);
             field.set(this.object, object);
         } catch (SecurityException | IllegalArgumentException | IllegalAccessException e) {
             ExceptionUtils.throwException(e);
         }
+        return this;
     }
 
-    public <O> ReflObject<O> obtainField(String name) {
-        return obtainField(getField(name));
+    /**
+     * Sets field.
+     *
+     * @param <O>    the type parameter
+     * @param type   the type
+     * @param object the object
+     * @return this refl object
+     */
+    public <O> @NotNull ReflObject<T> setField(@NotNull Class<?> type, @Nullable O object) {
+        if (this.object == null) return this;
+        Field field = getFieldNameless(type);
+        try {
+            field.setAccessible(true);
+            field.set(this.object, object);
+        } catch (SecurityException | IllegalArgumentException | IllegalAccessException e) {
+            ExceptionUtils.throwException(e);
+        }
+        return this;
     }
 
-    public <O> ReflObject<O> obtainFieldNameless(Class<?> type) {
-        return obtainField(getFieldNameless(type));
+    /**
+     * Gets field.
+     *
+     * @param name the name
+     * @return the field
+     */
+    public @NotNull Field getField(@NotNull String name) {
+        try {
+            Field field = ReflectionUtils.getField(objectClass, name);
+            if (field == null) throw new NoSuchFieldException(String.format(FIELD_NOT_FOUND, name, objectClass));
+            return field;
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public <O> ReflObject<O> obtainFieldNameless(String typeName) {
-        return obtainField(getFieldNameless(typeName));
+    /**
+     * Gets field nameless.
+     *
+     * @param typeName the type name
+     * @return the field
+     */
+    public @NotNull Field getFieldNameless(@NotNull String typeName) {
+        try {
+            Field field = ReflectionUtils.getFieldNameless(objectClass, typeName);
+            if (field == null) throw new NoSuchFieldException(String.format(FIELD_TYPE_NOT_FOUND, typeName, objectClass));
+            return field;
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private <O> ReflObject<O> obtainField(Field field) {
+    /**
+     * Gets field nameless.
+     *
+     * @param type the type
+     * @return the field
+     */
+    public @NotNull Field getFieldNameless(@NotNull Class<?> type) {
+        try {
+            Field field = ReflectionUtils.getField(objectClass, type);
+            if (field == null) throw new NoSuchFieldException(String.format(FIELD_TYPE_NOT_FOUND, type, objectClass));
+            return field;
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Gets the corresponding object wrapped in a {@link ReflObject} from the specified field.
+     *
+     * @param <O>  the type parameter
+     * @param name the name
+     * @return the {@link ReflObject} containing the field object
+     */
+    public <O> @NotNull ReflObject<O> getFieldReflObject(@NotNull String name) {
+        return getFieldReflObject(getField(name));
+    }
+
+    /**
+     * Gets the corresponding object wrapped in a {@link ReflObject} from the specified field.
+     *
+     * @param <O>  the type parameter
+     * @param type the type
+     * @return the {@link ReflObject} containing the field object
+     */
+    public <O> @NotNull ReflObject<O> getFieldReflObjectNameless(@NotNull Class<?> type) {
+        return getFieldReflObject(getFieldNameless(type));
+    }
+
+    /**
+     * Gets the corresponding object wrapped in a {@link ReflObject} from the specified field.
+     *
+     * @param <O>      the type parameter
+     * @param typeName the type name
+     * @return the {@link ReflObject} containing the field object
+     */
+    public <O> @NotNull ReflObject<O> getFieldReflObjectNameless(@NotNull String typeName) {
+        return getFieldReflObject(getFieldNameless(typeName));
+    }
+
+    private @NotNull <O> ReflObject<O> getFieldReflObject(@NotNull Field field) {
         Object obj = object == null ? objectClass : object;
         try {
             field.setAccessible(true);
             return new ReflObject<>((O) field.get(obj));
         } catch (IllegalAccessException e) {
             ExceptionUtils.throwException(e);
-            return null;
+            throw new IllegalArgumentException("Unreachable code");
         }
     }
 
-    public <O> O getFieldObject(String name) {
-        return (O) obtainField(name).getObject();
+    /**
+     * Gets the corresponding object from the specified field.
+     *
+     * @param <O>  the type parameter
+     * @param name the name
+     * @return the field object
+     */
+    public <O> @Nullable O getFieldObject(@NotNull String name) {
+        return (O) getFieldReflObject(name).getObject();
     }
 
-    public <O> O getFieldObjectNameless(Class<?> type) {
-        return (O) obtainFieldNameless(type).getObject();
+    /**
+     * Gets the corresponding object from the specified field.
+     *
+     * @param <O>  the type parameter
+     * @param type the type
+     * @return the field object nameless
+     */
+    public <O> @Nullable O getFieldObjectNameless(@NotNull Class<?> type) {
+        return (O) getFieldReflObjectNameless(type).getObject();
     }
 
-    public <O> O getFieldObjectNameless(String typeName) {
-        return (O) obtainFieldNameless(typeName).getObject();
+    /**
+     * Gets the corresponding object from the specified field.
+     *
+     * @param <O>      the type parameter
+     * @param typeName the type name
+     * @return the field object nameless
+     */
+    public <O> @Nullable O getFieldObjectNameless(@NotNull String typeName) {
+        return (O) getFieldReflObjectNameless(typeName).getObject();
     }
 
-    public List<Field> getFields() {
+    /**
+     * Gets the corresponding object from the specified field.
+     *
+     * @param <O>   the type parameter
+     * @param field the field
+     * @return the field object
+     */
+    public <O> @Nullable O getFieldObject(@NotNull Field field) {
+        return (O) getFieldReflObject(field).getObject();
+    }
+
+    /**
+     * Gets fields.
+     *
+     * @return the fields
+     */
+    public @NotNull List<Field> getFields() {
         return ReflectionUtils.getFields(object == null ? objectClass : object);
     }
 
