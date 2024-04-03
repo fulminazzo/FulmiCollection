0b6b543c5ff18c49b15bbcf069be43509581dcb2
Fulminazzo <fulminazzo21@gmail.com>
2024-04-03 20:24:55 +0200

    Created [structures](src/main/java/it/fulminazzo/fulmicollection/structures) package. Added [Tuple](src/main/java/it/fulminazzo/fulmicollection/structures/Tuple.java) type. Added [Triple](src/main/java/it/fulminazzo/fulmicollection/structures/Triple.java) type. Added `ReflectionUtils#setAccessible(Object)` function. Added `ReflectionUtils#get(Object, Field)` function. Added [FieldEquable](src/main/java/it/fulminazzo/fulmicollection/objects/FieldEquable.java) type. Fixed enums not being displayed in `Printable#convertToJson`. Fixed primitive parameters in constructors not being detected.

--- src/test/java/it/fulminazzo/fulmicollection/objects/FieldEquableTest.java
+++ src/test/java/it/fulminazzo/fulmicollection/objects/FieldEquableTest.java
@@ -1,11 +1,11 @@
 package it.fulminazzo.fulmicollection.objects;
 
-import net.bytebuddy.implementation.bind.annotation.SuperMethod;
 import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class FieldEquableTest {
 
