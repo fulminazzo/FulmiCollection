b5c44bba14acf0b779a4a7a14cd5582f0a32a33f
Fulminazzo <fulminazzo21@gmail.com>
2024-04-16 22:56:00 +0200

    Created NullableSinglet object

---
+++ src/main/java/it/fulminazzo/fulmicollection/structures/tuples/NullableSinglet.java
@@ -0,0 +1,34 @@
+package it.fulminazzo.fulmicollection.structures.tuples;
+
+/**
+ * An implementation of {@link Singlet} that allows null objects to be passed as values.
+ *
+ * @param <T> the type parameter
+ */
+public class NullableSinglet<T> extends Singlet<T> {
+    private boolean present;
+
+    @Override
+    public void setValue(T value) {
+        this.present = true;
+        super.setValue(value);
+    }
+
+    /**
+     * Unsets the current value.
+     */
+    public void unsetValue() {
+        setValue(null);
+        this.present = false;
+    }
+
+    @Override
+    public boolean hasValue() {
+        return this.present;
+    }
+
+    @Override
+    public boolean isPresent() {
+        return this.present;
+    }
+}
