558d92fafb928f56600acc895e359e57ef52bf8e
Fulminazzo <fulminazzo21@gmail.com>
2024-04-16 19:14:07 +0200

    Reworked `setAccessible` and `get` methods to return a non-empty optional in case of success

--- src/main/java/it/fulminazzo/fulmicollection/objects/Refl.java
+++ src/main/java/it/fulminazzo/fulmicollection/objects/Refl.java
@@ -66,7 +66,7 @@ public class Refl<T> {
     public Refl(final @NotNull Class<T> objectClass, final Class<?> @Nullable [] parameterTypes, Object @Nullable ... parameters) {
         try {
             Constructor<T> constructor = ReflectionUtils.getConstructor(objectClass, parameterTypes);
-            this.object = ReflectionUtils.setAccessible(constructor).newInstance(parameters);
+            this.object = setAccessible(constructor).newInstance(parameters);
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
             ExceptionUtils.throwException(e);
             throw new IllegalStateException("Unreachable code");
@@ -180,7 +180,7 @@ public class Refl<T> {
         try {
             Field finalField = field;
             field = getField(() -> finalField);
-            ReflectionUtils.setAccessible(field).set(this.object, value);
+            setAccessible(field).set(this.object, value);
             return this;
         } catch (IllegalAccessException e) {
             throw new RuntimeException(e);
@@ -608,7 +608,7 @@ public class Refl<T> {
                                         final Class<?> @Nullable [] paramTypes, final Object @Nullable ... parameters) {
         try {
             final Method method = getMethod(returnType, name, paramTypes);
-            return (O) ReflectionUtils.setAccessible(method).invoke(this.object, parameters);
+            return (O) setAccessible(method).invoke(this.object, parameters);
         } catch (IllegalAccessException | InvocationTargetException e) {
             ExceptionUtils.throwException(e);
             throw new IllegalStateException("Unreachable code");
@@ -969,6 +969,11 @@ public class Refl<T> {
                 }
             return output + "\n}";
         });
+    }
+
+    private static <T extends AccessibleObject> T setAccessible(final @NotNull T object) {
+        return ReflectionUtils.setAccessible(object).orElseThrow(() ->
+                new IllegalArgumentException(String.format("Could not set '%s' to accessible", object)));
     }
 
     @Override
