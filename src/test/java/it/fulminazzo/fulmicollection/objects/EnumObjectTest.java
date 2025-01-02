621b774e8551dc3b79e67f1fa4b50f2063052c7c
Fulminazzo <fulminazzo21@gmail.com>
2025-01-02 12:34:11 +0100

    Added tests for ordinal method with composedValues support function

--- src/test/java/it/fulminazzo/fulmicollection/objects/EnumObjectTest.java
+++ src/test/java/it/fulminazzo/fulmicollection/objects/EnumObjectTest.java
@@ -1,8 +1,30 @@
 package it.fulminazzo.fulmicollection.objects;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class EnumObjectTest {
+
+    private static Object[][] composedValues(Function<MockEnum, Object[]> mapFunction) {
+        return Arrays.stream(MockEnum.values()).map(mapFunction).toArray(Object[][]::new);
+    }
+
+    private static Object[][] ordinalValues() {
+        AtomicInteger i = new AtomicInteger();
+        return composedValues(e -> new Object[]{e, i.getAndIncrement()});
+    }
+
+    @ParameterizedTest
+    @MethodSource("ordinalValues")
+    void testOrdinal(MockEnum mockEnum, int ordinal) {
+        assertEquals(ordinal, mockEnum.ordinal());
+    }
 
     static class MockEnum extends EnumObject {
         public static final MockEnum FIRST = new MockEnum();
